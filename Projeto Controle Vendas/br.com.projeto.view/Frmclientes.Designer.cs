10df8bd11a5e739eae9368d6e7b46bd26448e47e
Hugo S. Dias <hugo_sdias@hotmail.com>
2022-10-10 22:21:58 -0300

    ajuste tamanho da tela

--- Projeto Controle Vendas/br.com.projeto.view/Frmclientes.Designer.cs
+++ Projeto Controle Vendas/br.com.projeto.view/Frmclientes.Designer.cs
@@ -77,7 +77,7 @@
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(905, 88);
+            this.panel1.Size = new System.Drawing.Size(809, 88);
             this.panel1.TabIndex = 0;
             // 
             // label1
@@ -455,7 +455,7 @@
             this.button1.BackColor = System.Drawing.SystemColors.Highlight;
             this.button1.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.Color.White;
-            this.button1.Location = new System.Drawing.Point(96, 456);
+            this.button1.Location = new System.Drawing.Point(85, 456);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(137, 60);
             this.button1.TabIndex = 2;
@@ -502,7 +502,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(905, 535);
+            this.ClientSize = new System.Drawing.Size(809, 535);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
